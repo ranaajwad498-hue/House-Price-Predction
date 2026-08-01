00bfc121f56274449e6d32b2a2ebfe5d91d86395
Muhammad Ajwad Rana <ranaajwad498@gmail.com>
2026-08-02 01:13:28 +0500

    Cleaned CSV file

--- price_prediction.py
+++ price_prediction.py
@@ -1,3 +1,4 @@
+import numpy as np
 import pandas as pd
 
 class priceprediction:
@@ -7,18 +8,14 @@ class priceprediction:
 
     def load_data(self):
         self.raw_data= pd.read_csv("house_prices_messy_dataset.csv")
-        # print(self.raw_data.head())
-        # print("Inforamtion of Data Set:",self.raw_data.info())
+        print(self.raw_data.head())
+        print("Inforamtion of Data Set:",self.raw_data.info())
       
 
     def cleaneing_data(self):
         self.df = self.raw_data
         # print("Duplicate Records In Data sets....")
         self.df.drop_duplicates()
-        self.df["Location"] = self.df["Location"].replace(["SUBURBS",'downtown'],['Suburbs' ,'Downtown'])
-        self.df["Condition"]=self.df["Condition"].replace(['FAIR','good'],['Fair','Good'])
-        self.df["Has_Pool"]= self.df["Has_Pool"].replace(['Y','N',"TRUE","FALSE"],["Yes","No","Yes","No"])
-        print(self.df["Has_Pool"].unique())
         print("Filling Null Values....")
         float_col=self.df.select_dtypes(include="float64")
         for col in float_col:
@@ -27,8 +24,21 @@ class priceprediction:
         str_col= self.df.select_dtypes(include="str")
         for col in str_col:
             self.df[col]=self.df[col].fillna(self.df[col].mode()[0])
-        print("Total Null Values in Data Set.....",self.df.isnull().sum())
-        print(self.df.dtypes)
+
+        self.df["Location"] = self.df["Location"].replace(["SUBURBS",'downtown'],['Suburbs' ,'Downtown'])
+        self.df["Condition"]=self.df["Condition"].replace(['FAIR','good'],['Fair','Good'])
+        self.df["Has_Pool"]= self.df["Has_Pool"].replace(['Y','N',"TRUE","FALSE"],["Yes","No","Yes","No"])
+        self.df["Bathrooms"]= self.df["Bathrooms"].replace([1.5, 2.5, 3.5,4.5],[2,3,4,5])
+        self.df["Area_SqFt"] = self.df["Area_SqFt"].astype(str).str.extract(r'(\d+\.?\d*)')[0].astype(float)
+        self.df["Area_SqFt"]=self.df["Area_SqFt"].fillna(self.df["Area_SqFt"].median())
+        self.df["Bedrooms"] = self.df["Bedrooms"].clip(lower=0, upper=5)
+        self.df["Price"] = self.df["Price"].clip(lower=0, upper=900000)
+        print("Checking Null Values.....",self.df.isnull().sum())
+        self.df.to_csv("Cleaned Data.csv",index=False)
+
+
+        
+
 
 
 
